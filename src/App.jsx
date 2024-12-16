849370feb702ad7c8892262276a39861ae61c22d
Alena-Solo <bazarkina.alena2020@gmail.com>
2024-12-16 22:44:18 +0100

    options

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
+import Description from "./components/Description/Description";
+import "./styles.css";
+
+const App = () => {
+  const [feedback, setFeedback] = useState(
+    () =>
+      JSON.parse(localStorage.getItem("feedback")) || {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+  );
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback;
+  ?Math.round((feedback.good / totalFeedback) * 100)
+  : 0;
+
+useEffect(() => {
+  localStorage.setItem("feedback", JSON.stringify(feedback));
+
+}, [feedback]);
+
+const updateFeedback = (feedbackType) => {
+  setFeedback((prevFeedback) => ({
+    ...prevFeedback,
+    [feedbackType]: prevFeedback[feedbackType] + 1,
+  }));
+};
+
+const restFeedback = () => {
+  setFeedback({ good: 0, neutral: 0, bad: 0 });
+};
+
+return (
+  <div>
+    <hi>Sip Happens Café</hi>
+    <p>Please leave your feedback about our service by selecting one of the
+      options below.</p>
+    <Options
+      onLeaveFeedback={updateFeedback}
+      onRest={restFeedback}
+      totalFeedback={totalFeedback}
+    />
+    {totalFeedback > 0 ? (
+      <Feedback
+        good={feedback.good}
+        neutral={feedback.neutral}
+        bad={feedback.bad}
+        totalFeedback={totalFeedback}
+        positiveFeedback={positiveFeedback}
+      />
+    ) : (
+      <Notification message="No feedback given" />
+    )}
+  </div>
+);
+};
+
+export default App;
